2616f5de99ec7333ec32d165bff573422165edd9
Alexander Nikolov <alexander.nikolov@absolutns.com>
2019-01-03 23:37:24 +0200

    get games to join

--- src/main/java/com/fmiproject/login/GameController.java
+++ src/main/java/com/fmiproject/login/GameController.java
@@ -2,6 +2,7 @@ package com.fmiproject.login;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,13 +19,14 @@ import org.springframework.web.client.RestTemplate;
 import com.fmiproject.login.dto.CheckGameStatusResponseDTO;
 import com.fmiproject.login.dto.CreateGameDTO;
 import com.fmiproject.login.dto.ListGames;
+import com.fmiproject.login.dto.ListGames.GamesDTO;
 import com.fmiproject.login.dto.ResponseDTO;
 
 @RestController
 public class GameController {
 	
 	private final static String START_GAME = "start";
-        private final static String JOIN_GAME = "join";
+	private final static String JOIN_GAME = "join";
 	
 	@Autowired
 	private RestTemplate restTemplate;
@@ -35,16 +37,16 @@ public class GameController {
 		String response = "";
 		if (action.equals(START_GAME)) {
 			RequestEntity<CreateGameDTO> request = RequestEntity
-				     .post(new URI("https://ug-game-api.azurewebsites.net/api/game"))
-				     .accept(MediaType.APPLICATION_JSON)
-				     .body(new CreateGameDTO(userid, role));
+					.post(new URI("https://ug-game-api.azurewebsites.net/api/game")).accept(MediaType.APPLICATION_JSON)
+					.body(new CreateGameDTO(userid, role));
 			ResponseEntity<ResponseDTO> responseDTO = restTemplate.exchange(request, ResponseDTO.class);
 			response = responseDTO.getBody().getData().getGame().getGameID();
-		} else if (action.equals(JOIN_GAME)) {             
-                        ResponseEntity<ListGames> game = restTemplate.getForEntity(
-                                        new URI("https://ug-game-api.azurewebsites.net/api/games/to_join"), ListGames.class);
-                                        response = ListGames.games.get(0).getGameID();
-                }
+		} else if (action.equals(JOIN_GAME)) {
+			ResponseEntity<ListGames> responseDTO = restTemplate
+					.getForEntity(new URI("https://ug-game-api.azurewebsites.net/api/games/to_join"), ListGames.class);
+			List<GamesDTO> gamesList = responseDTO.getBody().getData().getGames();
+			response = gamesList.get(gamesList.size() - 1).getGameID();
+		}
 		return response;
 	}
 	
--- src/main/java/com/fmiproject/login/dto/ListGames.java
+++ src/main/java/com/fmiproject/login/dto/ListGames.java
@@ -1,21 +1,54 @@
 package com.fmiproject.login.dto;
 
-import java.util.*;
+import java.util.List;
 
 public class ListGames {
-	
-        public static List<GamesDTO> games;
-        
-        public static class GamesDTO { 
-                private String gameID;
-		private Long stateID;
-               
-                public String getGameID() {
-                    return gameID;
-                }   
 
-                public Long getStateID() {
-                    return stateID;
-                }
-        }
+	private GameData data;
+
+	public static class GameData {
+		private List<GamesDTO> games;
+
+		/**
+		 * @return the games
+		 */
+		public List<GamesDTO> getGames() {
+			return games;
+		}
+
+		/**
+		 * @param games the games to set
+		 */
+		public void setGames(List<GamesDTO> games) {
+			this.games = games;
+		}
+	}
+
+	public static class GamesDTO {
+		private String gameID;
+		private Long stateID;
+
+		public String getGameID() {
+			return gameID;
+		}
+
+		public Long getStateID() {
+			return stateID;
+		}
+	}
+
+	/**
+	 * @return the data
+	 */
+	public GameData getData() {
+		return data;
+	}
+
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(GameData data) {
+		this.data = data;
+	}
+
 }
